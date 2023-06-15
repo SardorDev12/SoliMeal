2abb5e2ff4b89127b0887927c278b21778e80413
Sardor <sardoribnfarhod@gmail.com>
2023-06-15 14:17:39 +0500

    UI Desktop version

--- script.js
+++ script.js
@@ -14,3 +14,41 @@
 //   }
 // };
 // showRecipe();
+
+// ********** Bookmarks toggle ***********
+document.querySelector(".bookmarks").addEventListener("click", () => {
+  document.querySelector(".bookmarks-content").classList.toggle("show");
+});
+
+// *********** Modal **********
+const addRecipeForm = document.querySelector(".add-recipe-form");
+const bgEffect = document.querySelector(".bg-effect");
+window.addEventListener("click", (e) => {
+  if (e.target.classList.contains("add-recipe-btn")) {
+    addRecipeForm.classList.add("show");
+    bgEffect.classList.add("show");
+  } else if (e.target.classList.contains("close-btn")) {
+    addRecipeForm.classList.remove("show");
+    bgEffect.classList.remove("show");
+  }
+});
+
+// Servings counter
+const servingsBtns = document.querySelectorAll(".servings-btns button");
+let servingsNumberEl = document.querySelector(".servings-number");
+
+let servingsNumber = parseInt(
+  document.querySelector(".servings-number").innerText
+);
+
+servingsBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.classList.contains("inc") && servingsNumber >= 0) {
+      servingsNumber++;
+      servingsNumberEl.innerText = servingsNumber;
+    } else if (e.target.classList.contains("dec") && servingsNumber > 0) {
+      servingsNumber--;
+      servingsNumberEl.innerText = servingsNumber;
+    }
+  });
+});
